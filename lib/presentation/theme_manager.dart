b4d7753c03f545d62840ce22bffd0f3cc24b7b24
Ahmed Mohamed <71014427+ahmedMohamedAbdu@users.noreply.github.com>
2022-10-31 17:37:36 +0200

    appBar

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:tut_application/presentation/color_manager.dart';
+import 'package:tut_application/presentation/font_manager.dart';
+import 'package:tut_application/presentation/styles_manager.dart';
 import 'package:tut_application/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
@@ -10,15 +11,24 @@ ThemeData getApplicationTheme() {
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary,
 
     //carView theme
     cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
-      elevation:AppSize.s4,
+      elevation: AppSize.s4,
     ),
 
     //appBar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle:
+          getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+    ),
 
     //buttonTheme
 

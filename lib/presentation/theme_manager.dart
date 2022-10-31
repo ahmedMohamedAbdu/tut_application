90cb5e9bd435defef2090e2761883e990462f5ef
Ahmed Mohamed <71014427+ahmedMohamedAbdu@users.noreply.github.com>
2022-10-31 17:28:14 +0200

    card theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tut_application/presentation/color_manager.dart';
+import 'package:tut_application/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // mainColors
+    // mainColors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
 
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1
+    //carView theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation:AppSize.s4,
+    ),
 
-      //carView theme
+    //appBar theme
 
-      //appBar theme
+    //buttonTheme
 
-      //buttonTheme
+    //textTheme
 
-      //textTheme
-
-      //inputDecorationTheme(textFromField)
-
-      );
+    //inputDecorationTheme(textFromField)
+  );
 }

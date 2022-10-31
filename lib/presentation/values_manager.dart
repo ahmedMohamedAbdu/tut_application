a13f03d59d0bb94ed00aec8b49ae97d09791bf3d
Ahmed Mohamed <71014427+ahmedMohamedAbdu@users.noreply.github.com>
2022-10-31 15:54:31 +0200

    values

--- lib/presentation/values_manager.dart
+++ lib/presentation/values_manager.dart
@@ -18,3 +18,13 @@ class AppPadding
   static const double p18 = 18.0;
   static const double p20 = 20.0;
 }
+class AppSize
+{
+  static const double s8  = 8.0;
+  static const double s10 = 10.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+}

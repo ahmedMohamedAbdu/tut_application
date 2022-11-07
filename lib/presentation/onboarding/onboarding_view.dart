091993b6b13862fb2cb36a1d9c1b05d045772bea
Ahmed Mohamed <71014427+ahmedMohamedAbdu@users.noreply.github.com>
2022-11-08 00:40:35 +0200

    onboarding one

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tut_application/presentation/resources/assets_manager.dart';
 import 'package:tut_application/presentation/resources/color_manager.dart';
+import 'package:tut_application/presentation/resources/strings_manager.dart';
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({Key? key}) : super(key: key);
@@ -9,11 +12,54 @@ class OnboardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnboardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onboardingSubTitle1,
+            AppStrings.onboardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onboardingSubTitle2,
+            AppStrings.onboardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onboardingSubTitle3,
+            AppStrings.onboardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onboardingSubTitle4,
+            AppStrings.onboardingSubTitle4, ImageAssets.onboardingLogo4),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorManager.grey,
-      body: const Center(child: Text("welcome")),
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder
+        (
+        controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged:(index)
+          {
+            setState(() {
+              _currentIndex = index;
+            });
+          } ,
+          itemBuilder: (context,index)
+          {
+
+          }
+      ),
     );
   }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
